bfcd4bcec30fb04a5e2234def8d8a51c8593a7ff
kathuria, siddharth <skathuria@cvent.com>
2020-10-05 19:48:00 +0530

    first commit

---
+++ commands/mute.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: 'mute',
+    description: 'This command mutes everyone in a voice channel.',
+    async execute(message, args){
+        console.log(args);
+        var channel_name = args.join(" ");
+        console.log(channel_name);
+        const vc = message.guild.channels.cache.filter((channel) => channel.name==channel_name);
+        console.log(vc);
+        for (const [channelID, channel] of vc) {
+            for (const [memberID, member] of channel.members) {
+                if(member.voice.mute){
+                    message.channel.send(`Unmuting ${member.user.username}`)
+                    member.voice.setMute(false)
+                }else {
+                    message.channel.send(`Muting ${member.user.username}`)
+                    member.voice.setMute(true)
+                }
+            }
+          }
+    }
+}
---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js')
+const fs = require('fs');
+
+const client = new Discord.Client();
+
+const prefix = '!';
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command)
+}
+
+client.once('ready', () => {
+    console.log('Shush is online!')
+});
+
+client.on('message', (message) => {
+    if(!(message.content.startsWith(prefix)) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'mute'){
+        client.commands.get('mute').execute(message, args);
+    }
+    
+});
+
+client.login('NzU5NzQ5MTk4NjY0NDMzNjY1.X3CBlA.Va12PLy0GyRcgKl_ZDyeyaYGnwM');
