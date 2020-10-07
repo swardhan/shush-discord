7eac1719ab0b842921617bbe5a17b870ef0b2425
Saumya Wardhan <saumyawardhan1@gmail.com>
2020-10-07 19:12:07 +0530

    Merge Resolve 2

--- commands/mute.js
+++ commands/mute.js
@@ -2,18 +2,6 @@ module.exports = {
     name: 'mute',
     description: 'This command mutes everyone in a voice channel.',
     async execute(message, args){
-<<<<<<< HEAD
-        const vc = message.guild.channels.cache.filter((channel) => channel.id==args[0]);
-        for (const [channelID, channel] of vc) {
-            for (const [memberID, member] of channel.members) {
-                if(member.voice.mute){
-                    message.channel.send(`Unmuting ${member.user.username}`)
-                    member.voice.setMute(false)
-                }else {
-                    message.channel.send(`Muting ${member.user.username}`)
-                    member.voice.setMute(true)
-                }
-=======
         console.log(args);
         var channel_name = args.join(" ");
         console.log(channel_name);
@@ -23,7 +11,6 @@ module.exports = {
             for (const [memberID, member] of channel.members) {
                     message.channel.send(`Muting ${member.user.username}`)
                     member.voice.setMute(true)
->>>>>>> 6c14b6183a334ea7cb2cec54e708127ea29ef1b7
             }
           }
     }
--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const Discord = require('discord.js')
 const fs = require('fs');
-<<<<<<< HEAD
-
-const client = new Discord.Client();
-
-const prefix = '--';
-=======
 const config = require('./config.js');
 
 
 const client = new Discord.Client();
 
 const prefix = '!';
->>>>>>> 6c14b6183a334ea7cb2cec54e708127ea29ef1b7
 
 client.commands = new Discord.Collection();
 
@@ -29,23 +22,9 @@ client.once('ready', () => {
 
 client.on('message', (message) => {
     if(!(message.content.startsWith(prefix)) || message.author.bot) return;
-<<<<<<< HEAD
-
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if(command === 'mute'){
-        client.commands.get('mute').execute(message, args);
-    }
-    
-});
-
-client.login('NzU5NzQ5MTk4NjY0NDMzNjY1.X3CBlA.mGIDIkDxAzbhMeF-4q5K6tXGd8A');
-=======
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
     client.commands.get(command).execute(message, args);
 });
 
 client.login(config.key);
->>>>>>> 6c14b6183a334ea7cb2cec54e708127ea29ef1b7
